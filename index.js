9e03bb9cc41d025b0c3704c88d679fbbc22e57b4
Dibyanshu Kumar <dibyanshukumar05@gmail.com>
2024-01-10 23:05:21 +0530

    Uploding Ongoing Progress to github from local repo.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// document.querySelector(".link2").addEventListener("click", function(){
+//     location.href = "https://github.com/Dibyanshu502";
+// });
+
+
+document.getElementById("link2id").onclick = function () {
+    location.href = "https://github.com/Dibyanshu502";
+};
